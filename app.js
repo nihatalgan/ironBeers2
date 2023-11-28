78ba0492660dc4de3a0f60b29234d3dcacc90095
burak algan <burakalgan@gmail.com>
2023-11-28 21:11:17 +0100

    done partials

--- app.js
+++ app.js
@@ -15,11 +15,34 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Register the location for handlebars partials here:
 
 // ...
+// hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      // console.log('Beers from the database: ', beersFromApi);
+      return beersFromApi;
+    })
+    .then(beersFromApi => res.render('beers', { data: beersFromApi }))
+    .catch(error => console.log(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      // your magic happens here
+      console.log('Beers from the database: ', responseFromAPI);
+      res.render('random-beer', { data: responseFromAPI });
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
